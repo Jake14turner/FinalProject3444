e4c5699884b109a1174bf0890943594694e96d1f
Jake14turner <110646968+Jake14turner@users.noreply.github.com>
2024-12-11 15:19:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import streamlit as st
+from notifications import run_background_scheduler
+from database import create_assignments_table
+from user import run_reminder_in_background
+from user import initialize_user_assignments
+
 st.set_page_config(layout="wide")
+
+if "isLoggedIn" not in st.session_state:
+    st.session_state.isLoggedIn = False
+    
+
+if st.session_state.isLoggedIn and st.session_state.username:
+    initialize_user_assignments(st.session_state.username)
+# Start background reminder system
+    run_reminder_in_background()
+
+# Ensure the assignments table exists
+create_assignments_table()
+
+
+if "assignmentsArray" not in st.session_state:
+    st.session_state.assignmentsArray = []  # Placeholder for assignments array
+
+if st.session_state.assignmentsArray and st.session_state.username:
+    run_background_scheduler(st.session_state.assignmentsArray, st.session_state.username)
+
+
+
 
 registerPage = st.Page("registerPage.py", title="Register", icon=":material/person_add:")
 loginPage = st.Page("loginPage.py", title="Login", icon=":material/login:")
 homePage = st.Page("homePage.py", title="Home", icon=":material/calendar_today:")
-toDoPage = st.Page("todoPage.py", title="To Do List", icon=":material/note:")
+toDoPage = st.Page("toDoPage.py", title="To Do List", icon=":material/note:")
 customSchedulePage = st.Page("customSchedulePage.py", title="Custom Schedule", icon=":material/access_time:")
 tasksPage = st.Page("tasksPage.py", title="Tasks Page", icon=":material/add_circle:")
 logoutPage = st.Page("logoutPage.py", title="Logout Page", icon=":material/logout:")
