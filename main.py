48b359cd7a9e10904b862f6b2c317233aaec3537
Jake14turner <110646968+Jake14turner@users.noreply.github.com>
2024-12-11 15:21:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if st.session_state.assignmentsArray and st.session_state.username:
 registerPage = st.Page("registerPage.py", title="Register", icon=":material/person_add:")
 loginPage = st.Page("loginPage.py", title="Login", icon=":material/login:")
 homePage = st.Page("homePage.py", title="Home", icon=":material/calendar_today:")
-toDoPage = st.Page("toDoPage.py", title="To Do List", icon=":material/note:")
+toDoPage = st.Page("todoPage.py", title="To Do List", icon=":material/note:")
 customSchedulePage = st.Page("customSchedulePage.py", title="Custom Schedule", icon=":material/access_time:")
 tasksPage = st.Page("tasksPage.py", title="Tasks Page", icon=":material/add_circle:")
 logoutPage = st.Page("logoutPage.py", title="Logout Page", icon=":material/logout:")
